a3480825a3d6e574c74f1d62e8f6bbe1c1a7630a
David Liao <liao.canada@gmail.com>
2021-05-17 20:02:24 -0400

    Add favicon

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,4 @@
 aws s3 cp index.html s3://resume.davidliao.ca --cache-control="max-age=60"
-aws cloudfront create-invalidation --distribution-id E30X93DG4ZN4VX --paths /index.html
+aws s3 cp DavidLiao-Resume.pdf s3://resume.davidliao.ca --cache-control="max-age=60"
+aws s3 cp favicon.ico s3://resume.davidliao.ca --cache-control="max-age=60"
+aws cloudfront create-invalidation --distribution-id E30X93DG4ZN4VX --paths /*
